0840cd1cf975350eacd83e64c4e59dab32e5ea51
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-10-24 14:56:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum5/Data/DataForm.kt
@@ -0,0 +1,3 @@
+package com.example.praktikum5.Data
+
+data class DataForm(val sex: String = "")
---
+++ app/src/main/java/com/example/praktikum5/Data/DataSource.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum5.Data
+
+import com.example.praktikum5.R
+
+object Datasource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
